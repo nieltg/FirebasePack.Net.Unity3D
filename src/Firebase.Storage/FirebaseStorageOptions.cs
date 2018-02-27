08369d2729e42064f165f1d27120ec87c313d52f
Daniel <nieltansah@gmail.com>
2018-02-28 06:20:06 +0700

    Merge c221006a5a567e23be8c1b3e0ae09feca8d46c0f:src/Firebase.Storage to src/Firebase.Storage
    git-subpath-tag src/Firebase.Storage -> src/Firebase.Storage
     mainline: c221006a5a567e23be8c1b3e0ae09feca8d46c0f

---
+++ src/Firebase.Storage/FirebaseStorageOptions.cs
@@ -0,0 +1,26 @@
+﻿namespace Firebase.Storage
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public class FirebaseStorageOptions
+    {
+        /// <summary>
+        /// Gets or sets the method for retrieving auth tokens. Default is null.
+        /// </summary>
+        public Func<Task<string>> AuthTokenAsyncFactory
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets whether <see cref="TaskCanceledException"/> should be thrown when cancelling a running <see cref="FirebaseStorageTask"/>.
+        /// </summary>
+        public bool ThrowOnCancel
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ src/Firebase.Storage/FirebaseStorageReference.cs
@@ -0,0 +1,136 @@
+﻿namespace Firebase.Storage
+{
+    using Newtonsoft.Json;
+
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class FirebaseStorageReference
+    {
+        private const string FirebaseStorageEndpoint = "https://firebasestorage.googleapis.com/v0/b/";
+
+        private readonly FirebaseStorage storage;
+        private readonly List<string> children;
+
+        internal FirebaseStorageReference(FirebaseStorage storage, string childRoot)
+        {
+            this.children = new List<string>();
+
+            this.storage = storage;
+            this.children.Add(childRoot);
+        }
+
+        /// <summary>
+        /// Starts uploading given stream to target location.
+        /// </summary>
+        /// <param name="stream"> Stream to upload. </param>
+        /// <param name="cancellationToken"> Cancellation token which can be used to cancel the operation. </param>
+        /// <returns> <see cref="FirebaseStorageTask"/> which can be used to track the progress of the upload. </returns>
+        public FirebaseStorageTask PutAsync(Stream stream, CancellationToken cancellationToken)
+        {
+            return new FirebaseStorageTask(this.storage.Options, this.GetTargetUrl(), this.GetFullDownloadUrl(), stream, cancellationToken);
+        }
+
+        /// <summary>
+        /// Starts uploading given stream to target location.
+        /// </summary>
+        /// <param name="stream"> Stream to upload. </param>
+        /// <returns> <see cref="FirebaseStorageTask"/> which can be used to track the progress of the upload. </returns>
+        public FirebaseStorageTask PutAsync(Stream fileStream)
+        {
+            return this.PutAsync(fileStream, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Gets the url to download given file.
+        /// </summary>
+        public async Task<string> GetDownloadUrlAsync()
+        {
+            var url = this.GetDownloadUrl();
+            var resultContent = "N/A";
+
+            try
+            {
+                using (var http = await this.storage.Options.CreateHttpClientAsync().ConfigureAwait(false))
+                {
+                    var result = await http.GetAsync(url);
+                    resultContent = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    var data = JsonConvert.DeserializeObject<Dictionary<string, string>>(resultContent);
+
+                    result.EnsureSuccessStatusCode();
+
+                    return this.GetFullDownloadUrl() + data["downloadTokens"];
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new FirebaseStorageException(url, resultContent, ex);
+            }
+        }
+
+        /// <summary>
+        /// Deletes a file at target location.
+        /// </summary>
+        public async Task DeleteAsync()
+        {
+            var url = this.GetDownloadUrl();
+            var resultContent = "N/A";
+
+            try
+            {
+                using (var http = await this.storage.Options.CreateHttpClientAsync().ConfigureAwait(false))
+                {
+                    var result = await http.DeleteAsync(url).ConfigureAwait(false);
+
+                    resultContent = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                    result.EnsureSuccessStatusCode();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new FirebaseStorageException(url, resultContent, ex);
+            }
+        }
+
+        /// <summary>
+        /// Constructs firebase path to the file.
+        /// </summary>
+        /// <param name="name"> Name of the entity. This can be folder or a file name or full path.</param>
+        /// <example>
+        ///     storage
+        ///         .Child("some")
+        ///         .Child("path")
+        ///         .Child("to/file.png");
+        /// </example>
+        /// <returns> <see cref="FirebaseStorageReference"/> for fluid syntax. </returns>
+        public FirebaseStorageReference Child(string name)
+        {
+            this.children.Add(name);
+            return this;
+        }
+
+        private string GetTargetUrl()
+        {
+            return $"{FirebaseStorageEndpoint}{this.storage.StorageBucket}/o?name={this.GetEscapedPath()}";
+        }
+
+        private string GetDownloadUrl()
+        {
+            return $"{FirebaseStorageEndpoint}{this.storage.StorageBucket}/o/{this.GetEscapedPath()}";
+        }
+
+        private string GetFullDownloadUrl()
+        {
+            return this.GetDownloadUrl() + "?alt=media&token=";
+        }
+
+        private string GetEscapedPath()
+        {
+            return Uri.EscapeDataString(string.Join("/", this.children));
+        }
+    }
+}
